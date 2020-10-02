7d05068acbd9dba3fa52d0659d86847c319c36a8
Rafael Santana <rgs1990419@gmail.com>
2020-10-02 09:39:44 -0300

    Aplicação ES6 - Render em tela

--- ES6/src/main.js
+++ ES6/src/main.js
@@ -3,8 +3,11 @@ class App {
     // Inicializa a variável onde ficarão armazenados os repositórios adicionados
     this.repositories = [];
 
-    // Adiciona a referência para o elemento do <form />
+    // Adiciona a referência para o elemento <form />
     this.formEl = document.getElementById("repo-form");
+
+    // Adiciona a referência para o elemento <ul />
+    this.listEl = document.getElementById("repo-list");
 
     // Instancia a função registerHandlers da classe App
     this.registerHandlers();
@@ -26,7 +29,50 @@ class App {
       html_url: "http://github.com/rocketseat/rocketseat.com.br",
     });
 
-    console.log(this.repositories);
+    this.render();
+  }
+
+  // Vai apagar o conteúdo da lista e vai renderizar os itens do zero, percorrendo todo o array
+  render() {
+    this.listEl.innerHTML = ""; // Apaga contepudo da lista
+
+    // Percorrer o array de repos, sem fazer nenhuma alteração, apenas criando e renderizando os elementos em tela
+    this.repositories.forEach((repo) => {
+      // Cria elemento <img src={repo.avatar_url} />
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      // Cria elemento <strong>{repo.name}</strong>
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      // Cria elemento <p>{repo.description}</p>
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      // Cria elemento <a target="_blank">Acessar</a>
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      /** Cria elemento
+       * <li>
+       *    <img src={repo.avatar_url} />
+       *    <strong>{repo.name}</strong>
+       *    <p>{repo.description}</p>
+       *    <a target="_blank">Acessar</a>
+       * </li>
+       * */
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      // Adc. o listItemEl dentro o ul
+      this.listEl.appendChild(listItemEl);
+    });
   }
 }
 
